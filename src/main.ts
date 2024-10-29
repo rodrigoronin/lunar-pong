15b5ac96c49f2b686cd69caaa3c19eddcb8f8106
rodrigo ronin <liramailbox@gmail.com>
2024-10-29 16:08:54 -0300

    chore :tada:: canvas project structure

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.log("Game started!");
+
+const canvas: HTMLCanvasElement = document.getElementById(
+  "canvas"
+) as HTMLCanvasElement;
+const context: CanvasRenderingContext2D = canvas.getContext(
+  "2d"
+) as CanvasRenderingContext2D;
